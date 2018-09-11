7cda2187286ff7a8e1911667c0d93047b059b08f
Mateusz-Krupa <treestram@gmail.com>
2018-09-11 22:28:50 +0200

    fix for storybook

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,7 +4,7 @@ import './styles/Button.css';
 export default class Button extends Component {
   render() {
     return (
-      <div className="Button">
+      <div className="Button" onClick={this.props.onClick}>
         {this.props.children}
       </div>
     );
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,10 +4,7 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Welcome } from '@storybook/react/demo';
-import { Button } from '../src/components/Button/Button';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+import Button from '../src/components/Button/Button';
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
